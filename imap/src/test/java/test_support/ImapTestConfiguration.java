61ae68bf11cb088a6e42ab72081f467521151b68
subbotin <subbotin@haulmont.com>
2021-02-04 18:07:16 +0400

    Hibernate merge into master
    Rename eclipselink modules

--- imap/src/test/java/test_support/ImapTestConfiguration.java
+++ imap/src/test/java/test_support/ImapTestConfiguration.java
@@ -25,7 +25,7 @@ import io.jmix.data.DataConfiguration;
 import io.jmix.data.impl.JmixEntityManagerFactoryBean;
 import io.jmix.data.impl.JmixTransactionManager;
 import io.jmix.data.persistence.DbmsSpecifics;
-import io.jmix.dataeclipselink.DataEclipselinkConfiguration;
+import io.jmix.eclipselink.EclipselinkConfiguration;
 import io.jmix.imap.ImapConfiguration;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
@@ -42,7 +42,7 @@ import javax.sql.DataSource;
 
 @Configuration
 @PropertySource("classpath:/test_support/test-app.properties")
-@Import({CoreConfiguration.class, DataConfiguration.class, DataEclipselinkConfiguration.class,
+@Import({CoreConfiguration.class, DataConfiguration.class, EclipselinkConfiguration.class,
         ImapConfiguration.class})
 @EnableWebSecurity
 public class ImapTestConfiguration extends CoreSecurityConfiguration {
